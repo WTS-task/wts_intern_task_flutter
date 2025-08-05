59374530fbfdbf635bcd753dd44b1f1423bfc0f2
Aleksandr <alexkoh.x@gmail.com>
2025-08-05 14:28:14 +0300

    Поправил ProductItemCard

--- lib/features/catalog/presentation/view/widgets/product_card.dart
+++ lib/features/catalog/presentation/view/widgets/product_card.dart
@@ -2,24 +2,20 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:wts_task/core/constants/app_colors.dart';
 import 'package:wts_task/core/constants/constants.dart';
+import 'package:wts_task/core/widgets/custom_cached_image.dart';
 import 'package:wts_task/features/cart/presentation/view/screens/cart_screen.dart';
 import 'package:wts_task/features/catalog/presentation/view/product_detail_screen.dart';
 import 'package:wts_task/features/product/data/models/product.dart';
 
 class ProductItemCard extends StatelessWidget {
-  const ProductItemCard({
-    required this.item, required this.theme, super.key,
-  });
+  const ProductItemCard({required this.item, super.key});
 
   final Product item;
-  final ThemeData theme;
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16),
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
       elevation: 0.1,
       color: AppColors.background,
       child: InkWell(
@@ -28,11 +24,10 @@ class ProductItemCard extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              _buildImage(),
+              Expanded(flex: 2, child: _buildImage()),
               const SizedBox(height: 8),
-              _buildTitle(),
+              Flexible(child: _buildTitle()),
               const SizedBox(height: 8),
               _buildCartButton(context),
             ],
@@ -43,41 +38,13 @@ class ProductItemCard extends StatelessWidget {
   }
 
   Widget _buildImage() {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(12),
-      child: Container(
-        width: double.infinity,
-        color: AppColors.background,
-        child: CachedNetworkImage(
-          width: 64,
-          height: 64,
-          imageUrl: item.imageUrl ?? Constants.placeholderImageUrl,
-          progressIndicatorBuilder: (context, url, progress) {
-            return Center(
-              child: SizedBox(
-                width: 64,
-                height: 64,
-                child: CircularProgressIndicator.adaptive(
-                  value: progress.progress,
-                ),
-              ),
-            );
-          },
-          errorWidget: (context, url, error) {
-            return Icon(Icons.error, color: theme.colorScheme.error);
-          },
-        ),
-      ),
-    );
+    return CustomCachedImage(imageUrl: item.imageUrl);
   }
 
   Widget _buildTitle() {
     return Text(
       item.name ?? "Нет названия",
-      style: theme.textTheme.bodyLarge?.copyWith(
-        fontWeight: FontWeight.w500,
-      ),
-      maxLines: 3,
+      maxLines: 2,
       overflow: TextOverflow.ellipsis,
     );
   }
@@ -86,10 +53,7 @@ class ProductItemCard extends StatelessWidget {
     return Align(
       alignment: Alignment.bottomRight,
       child: IconButton(
-        icon: Icon(
-          Icons.shopping_basket_outlined,
-          color: theme.iconTheme.color,
-        ),
+        icon: const Icon(Icons.shopping_basket_outlined),
         onPressed: () => _openCartScreen(context),
       ),
     );
@@ -107,9 +71,7 @@ class ProductItemCard extends StatelessWidget {
   void _openCartScreen(BuildContext context) {
     Navigator.push(
       context,
-      MaterialPageRoute(
-        builder: (_) => const CartScreen(),
-      ),
+      MaterialPageRoute(builder: (_) => const CartScreen()),
     );
   }
 }
