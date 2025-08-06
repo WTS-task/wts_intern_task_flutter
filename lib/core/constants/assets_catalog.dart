f3bbb0a6612d47962a320b658f4337701074536f
Karina Malinina <malininakarina172@gmail.com>
2025-08-06 13:06:37 +0300

    Удалила ненужные файлы, перделала наследование

--- lib/core/constants/assets_catalog.dart
+++ lib/core/constants/assets_catalog.dart
@@ -1,11 +1,3 @@
 class AssetsCatalog {
   static const String logo = 'assets/images/the_shop_creative.jpg';
-
-  static const String headphonesImage = 'assets/images/headphones_image.png';
-  static const String avatarOneImage = 'assets/images/avatar_one_image.png';
-  static const String avatarTwoImage = 'assets/images/avatar_two_image.png';
-  static const String reviewsPersonOne = 'assets/images/reviews_person_one.png';
-  static const String reviewsPersonTwo = 'assets/images/reviews_person_two.png';
-  static const String reviewsPersonThree =
-      'assets/images/reviews_person_three.png';
 }
